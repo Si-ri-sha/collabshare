435f013140ac3b5ba8580df0044f275c6b5206a5
Siri TH <115623707+Si-ri-sha@users.noreply.github.com>
2025-01-18 21:18:36 +0530

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -26,7 +26,16 @@ router.post('/login', async (req, res) => {
 
   try {
     const user = await User.findOne({ email});
-    if (!user) return res.status(400).json({ message: 'Invalid credentials'}};
+    if (!user) return res.status(400).json({ message: 'Invalid credentials'});
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ message: 'invalid credentials'});
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIN : '1h' });
+    res.status(200).json({ token });
+ catch (error) {
+    res.status(500).json({ message: 'Error logging in' });
+    
   
     
 
