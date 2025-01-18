bd1824c7dad12aced3fff628422fdbe14ddfcb93
Siri TH <115623707+Si-ri-sha@users.noreply.github.com>
2025-01-18 21:13:54 +0530

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -12,6 +12,22 @@ router.post('/register', async (req, res) => {
     const userExists = await User.findOne({ email });
     if (userExists) return res.status(400).json({ message: 'User already exists' });
 
+    const newUser = new User({ name, email, password});
+    await newUser.save();
+    res.status(201).json({message: 'User registered successfully' });
+  } catch (error) {
+    res.status(500).json({message: 'Error registering user' });
+  }
+});
+
+//Login Route
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email});
+    if (!user) return res.status(400).json({ message: 'Invalid credentials'}};
+  
     
 
 
