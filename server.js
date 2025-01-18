b03a1073b23e382f289af97506dd9e44c863f509
Siri TH <115623707+Si-ri-sha@users.noreply.github.com>
2025-01-18 20:43:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,3 +6,20 @@ const dotenv = require('dotenv');
 dotenv.congif();
 const app = express();
 
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+//Database connection
+mongoose.connect(process.env.MONGO_URI, {
+  useNewURLParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch((err) => console.error('DB connection error:', err));
+
+
+//Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log('Server running on port ${PORT}'));
+
