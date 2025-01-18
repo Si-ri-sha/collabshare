1f5df1a4249a5673e92763ad552337772185c1d5
Siri TH <115623707+Si-ri-sha@users.noreply.github.com>
2025-01-18 20:37:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.congif();
+const app = express();
 
