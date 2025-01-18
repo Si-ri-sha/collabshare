4b05b4ea958d0bbce8a5a0ce957ec407ae1cfd13
Siri TH <115623707+Si-ri-sha@users.noreply.github.com>
2025-01-18 21:03:49 +0530

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,4 +4,14 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const router = express.Router();
 
+// Register Route
+router.post('/register', async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const userExists = await User.findOne({ email });
+    if (userExists) return res.status(400).json({ message: 'User already exists' });
+
+    
+
 
