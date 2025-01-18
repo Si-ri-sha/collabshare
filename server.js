2d910d7cb9bfd8e9423d34cb7bc8fa461df9d414
Siri TH <115623707+Si-ri-sha@users.noreply.github.com>
2025-01-18 20:44:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,6 @@ mongoose.connect(process.env.MONGO_URI, {
 .then(() => console.log('Connected to MongoDB'))
 .catch((err) => console.error('DB connection error:', err));
 
-
 //Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log('Server running on port ${PORT}'));
